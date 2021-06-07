c5fbb4b53e678f47ec450a4bd22fdfbde133ca1a
Isaac Tennant <isaactennant@Isaacs-MBP.lan>
2021-06-07 18:09:52 +0100

    basic auth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+       session[:user_id] = @user.id
+       redirect_to '/properties'
+    else
+       redirect_to '/login'
+    end 
+  end
+
+  def login
+  end
+
+  def welcome
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/welcome', notice: "Logged out!"
+  end
+end
